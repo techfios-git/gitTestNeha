5c4c9b444be3b0c39af71938b4c549193507b463
shihabamin <amin.khandaker@gmail.com>
2021-01-31 14:33:55 -0600

    2nd  commit

--- src/main/java/test/testtest.java
+++ src/main/java/test/testtest.java
@@ -1,5 +1,13 @@
 package test;
 
 public class testtest {
+	
+	public static void main(String[] args) {
+		printMe();
+	}
+	
+	public static void printMe() {
+		System.out.println("print");
+	}
 
 }
